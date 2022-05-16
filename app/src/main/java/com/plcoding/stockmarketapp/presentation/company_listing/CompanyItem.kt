b84308102b962d86a43591cd71dfdc5fb1c5974c
Alberto Muñoz <alberto.mqo@gmail.com>
2022-05-16 07:52:46 +0200

    Added DI and navigation

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listing/CompanyItem.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listing/CompanyItem.kt
@@ -1,10 +1,16 @@
 package com.plcoding.stockmarketapp.presentation.company_listing
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.plcoding.stockmarketapp.domain.model.CompanyListing
-
 
 @Composable
 fun CompanyItem(
@@ -13,5 +19,35 @@ fun CompanyItem(
 ) {
     Row(
         modifier = modifier,
-    )
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Text(
+                    text = company.name,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colors.onBackground,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = company.exchange,
+                    fontWeight = FontWeight.Light,
+                    color = MaterialTheme.colors.onBackground
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "(${company.symbol})",
+                fontStyle = FontStyle.Italic,
+                color = MaterialTheme.colors.onBackground
+            )
+        }
+    }
 }
